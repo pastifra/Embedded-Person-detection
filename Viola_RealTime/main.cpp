803c88d34faaa774a5daffcc5f8990429acf92b3
Francesco Pasti <107403732+frapasti@users.noreply.github.com>
2022-06-23 10:47:29 +0200

    Update main.cpp

--- Viola_RealTime/main.cpp
+++ Viola_RealTime/main.cpp
@@ -1,3 +1,8 @@
+/* FOR THE RASPBERRY PI 32-bit BULLSEYE OS
+/  This code achieves a real time person detection via the HOG detector
+/  provided by OpenCV on the target device Raspberry Pi with OS mentioned above
+/  In order to detect the camera connected to the Raspberry the gstreamer_pipeline is used */
+
 #include <opencv2/opencv.hpp>
 #include <opencv2/objdetect.hpp>
 #include <opencv2/highgui.hpp>
@@ -9,8 +14,7 @@
 using namespace std;
 using namespace cv;
 
-/// For the Raspberry Pi 32-bit Bullseye OS
-
+/*Pipeline parameters*/
 std::string gstreamer_pipeline(int capture_width, int capture_height, int framerate, int display_width, int display_height) {
     return
             " libcamerasrc ! video/x-raw, "
@@ -26,33 +30,31 @@ std::string gstreamer_pipeline(int capture_width, int capture_height, int framer
 int main()
 {
     //pipeline parameters
-    int capture_width = 640; //1280 ;
-    int capture_height = 480; //720 ;
+    int capture_width = 640;
+    int capture_height = 480;
     int framerate = 15 ;
-    int display_width = 640; //1280 ;
-    int display_height = 480; //720 ;
+    int display_width = 640;
+    int display_height = 480;
 
     //reset frame average
     std::string pipeline = gstreamer_pipeline(capture_width, capture_height, framerate,
                                               display_width, display_height);
     std::cout << "Using pipeline: \n\t" << pipeline << "\n\n\n";
 
-    /* Cascade classifiers inizialization; a series of weak lerners put into a cascade
+    /* Cascade classifier inizialization; a series of weak lerners put into a cascade
     /  one after the other, in this way they become a good strong classifier */
 
-    CascadeClassifier face_cascade;
     CascadeClassifier body_cascade;
 
     /* Load the classifiers from a "data" provided by openCV, so already trained
-    /  It is basetoolpoollikujmnhhytgbvfredcxswqazasdsdfdgffhgjhjklipiuyutreewd on Haar features that use the integral image for evaluation and are really fast to compute */
-    String face_cascade_name = "/home/francesco/opencv/data/haarcascades/haarcascade_frontalface_alt.xml";
+    /  It is based on Haar features that use the integral image for evaluation and are really fast to compute */
     String body_cascade_name = "/home/francesco/opencv/data/haarcascades/haarcascade_fullbody.xml";
-    face_cascade.load(face_cascade_name);
     body_cascade.load(body_cascade_name);
 
     /* Start the video capture with the gstreamer Pipeline */
     cv::VideoCapture cap(pipeline, cv::CAP_GSTREAMER);
-    if(!cap.isOpened()) {
+    if(!cap.isOpened()) 
+    {
         std::cout<<"Failed to open camera."<<std::endl;
         return (-1);
     }
