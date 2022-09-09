7527ba36bf8da0cd0216f1f96eda3aba1d623d7c
Francesco Pasti <107403732+frapasti@users.noreply.github.com>
2022-09-09 08:51:01 +0200

    Update yolo_v2.py

--- Custom_Architectures/yolo_v2.py
+++ Custom_Architectures/yolo_v2.py
@@ -1,83 +1,62 @@
-#-----------#
-#| YOLO V2 |#
-#-----------#
+from tensorflow.keras.layers import Dense, InputLayer, Dropout, Flatten, Reshape
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, GlobalMaxPooling2D, BatchNormalization
+from tensorflow.keras.regularizers import l2
+lrelu = tf.keras.layers.LeakyReLU(alpha=0.1)
 
-tf.keras.backend.clear_session()
+K.clear_session()
 
-yolo = Sequential()
+#---------#
+# YOLO v2 #
+#------ --#
+yolo =  Sequential()
 
-yolo.add(tf.keras.layers.Conv2D(32, (3, 3), padding="same", input_shape=(448,448,3), kernel_regularizer=l2(5e-4), use_bias = False))
-yolo.add(tf.keras.layers.BatchNormalization()) #Batch normalization needs to be executed before lrelu apparently 
-yolo.add(LeakyReLU(alpha=0.1))
-yolo.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2),strides = (2,2)))
+yolo.add(Conv2D(filters=16, kernel_size= (7, 7), strides=(1, 1), input_shape =(448, 448, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+yolo.add(BatchNormalization())
+yolo.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding = 'same'))
 
-yolo.add(tf.keras.layers.Conv2D(64, (3, 3), padding="same", kernel_regularizer=l2(5e-4), use_bias = False))
-yolo.add(tf.keras.layers.BatchNormalization()) #Batch normalization needs to be executed before lrelu apparently 
-yolo.add(LeakyReLU(alpha=0.1))
-yolo.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2),strides = (2,2)))
+yolo.add(Conv2D(filters=48, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+yolo.add(BatchNormalization())
+yolo.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding = 'same'))
 
-yolo.add(tf.keras.layers.Conv2D(128, (3, 3), padding="same", kernel_regularizer=l2(5e-4), use_bias = False))
-yolo.add(tf.keras.layers.BatchNormalization()) #Batch normalization needs to be executed before lrelu apparently 
-yolo.add(LeakyReLU(alpha=0.1))
-yolo.add(tf.keras.layers.Conv2D(64, (1, 1), padding="valid", kernel_regularizer=l2(5e-4), use_bias = False))
-yolo.add(tf.keras.layers.BatchNormalization()) #Batch normalization needs to be executed before lrelu apparently 
-yolo.add(LeakyReLU(alpha=0.1))
-yolo.add(tf.keras.layers.Conv2D(128, (3, 3), padding="same", kernel_regularizer=l2(5e-4), use_bias = False))
-yolo.add(tf.keras.layers.BatchNormalization()) #Batch normalization needs to be executed before lrelu apparently 
-yolo.add(LeakyReLU(alpha=0.1))
-yolo.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2),strides = (2,2)))
+yolo.add(Conv2D(filters=32, kernel_size= (1, 1), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+yolo.add(BatchNormalization())
+yolo.add(Conv2D(filters=64, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+yolo.add(BatchNormalization())
+yolo.add(Conv2D(filters=32, kernel_size= (1, 1), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+yolo.add(BatchNormalization())
+yolo.add(Conv2D(filters=64, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+yolo.add(BatchNormalization())
+yolo.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding = 'same'))
 
-yolo.add(tf.keras.layers.Conv2D(256, (3, 3), padding="same", kernel_regularizer=l2(5e-4), use_bias = False))
-yolo.add(tf.keras.layers.BatchNormalization()) #Batch normalization needs to be executed before lrelu apparently 
-yolo.add(LeakyReLU(alpha=0.1))
-yolo.add(tf.keras.layers.Conv2D(128, (1, 1), padding="valid", kernel_regularizer=l2(5e-4), use_bias = False))
-yolo.add(tf.keras.layers.BatchNormalization()) #Batch normalization needs to be executed before lrelu apparently 
-yolo.add(LeakyReLU(alpha=0.1))
-yolo.add(tf.keras.layers.Conv2D(256, (3, 3), padding="same", kernel_regularizer=l2(5e-4), use_bias = False))
-yolo.add(tf.keras.layers.BatchNormalization()) #Batch normalization needs to be executed before lrelu apparently 
-yolo.add(LeakyReLU(alpha=0.1))
-yolo.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2),strides = (2,2)))
+yolo.add(Conv2D(filters=64, kernel_size= (1, 1), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+yolo.add(BatchNormalization())
+yolo.add(Conv2D(filters=128, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+yolo.add(BatchNormalization())
+yolo.add(Conv2D(filters=64, kernel_size= (1, 1), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+yolo.add(BatchNormalization())
+yolo.add(Conv2D(filters=128, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+yolo.add(BatchNormalization())
+yolo.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding = 'same'))
 
-yolo.add(tf.keras.layers.Conv2D(512, (3, 3), padding="same", kernel_regularizer=l2(5e-4), use_bias = False))
-yolo.add(tf.keras.layers.BatchNormalization()) #Batch normalization needs to be executed before lrelu apparently 
-yolo.add(LeakyReLU(alpha=0.1))
-yolo.add(tf.keras.layers.Conv2D(256, (1, 1), padding="valid", kernel_regularizer=l2(5e-4), use_bias = False))
-yolo.add(tf.keras.layers.BatchNormalization()) #Batch normalization needs to be executed before lrelu apparently 
-yolo.add(LeakyReLU(alpha=0.1))
-yolo.add(tf.keras.layers.Conv2D(512, (3, 3), padding="same", kernel_regularizer=l2(5e-4), use_bias = False))
-yolo.add(tf.keras.layers.BatchNormalization()) #Batch normalization needs to be executed before lrelu apparently 
-yolo.add(LeakyReLU(alpha=0.1))
-yolo.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2),strides = (2,2)))
+yolo.add(Conv2D(filters=128, kernel_size= (1, 1), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+yolo.add(BatchNormalization())
+yolo.add(Conv2D(filters=256, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+yolo.add(BatchNormalization())
+yolo.add(Conv2D(filters=128, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+yolo.add(BatchNormalization())
+yolo.add(Conv2D(filters=256, kernel_size= (3, 3), strides=(2, 2), padding = 'same'))
 
-yolo.add(tf.keras.layers.Conv2D(1024, (3, 3), padding="same", kernel_regularizer=l2(5e-4), use_bias = False))
-yolo.add(tf.keras.layers.BatchNormalization()) #Batch normalization needs to be executed before lrelu apparently 
-yolo.add(LeakyReLU(alpha=0.1))
-yolo.add(tf.keras.layers.Conv2D(512, (1, 1), padding="valid", kernel_regularizer=l2(5e-4), use_bias = False))
-yolo.add(tf.keras.layers.BatchNormalization()) #Batch normalization needs to be executed before lrelu apparently 
-yolo.add(LeakyReLU(alpha=0.1))
-yolo.add(tf.keras.layers.Conv2D(1024, (3, 3), padding="same", kernel_regularizer=l2(5e-4), use_bias = False))
-yolo.add(tf.keras.layers.BatchNormalization()) #Batch normalization needs to be executed before lrelu apparently 
-yolo.add(LeakyReLU(alpha=0.1))
-yolo.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2),strides = (2,2)))
+yolo.add(Conv2D(filters=256, kernel_size= (3, 3), activation=lrelu, kernel_regularizer=l2(5e-4)))
+yolo.add(BatchNormalization())
+yolo.add(Conv2D(filters=256, kernel_size= (3, 3), activation=lrelu, kernel_regularizer=l2(5e-4)))
+yolo.add(BatchNormalization())
 
-yolo.add(tf.keras.layers.Conv2D(512, (3, 3), padding="same", kernel_regularizer=l2(5e-4), use_bias = False))
-yolo.add(tf.keras.layers.BatchNormalization()) #Batch normalization needs to be executed before lrelu apparently 
-yolo.add(LeakyReLU(alpha=0.1))
-yolo.add(tf.keras.layers.Conv2D(256, (3, 3), padding="same", kernel_regularizer=l2(5e-4), use_bias = False))
-yolo.add(tf.keras.layers.BatchNormalization()) #Batch normalization needs to be executed before lrelu apparently 
-yolo.add(LeakyReLU(alpha=0.1))
-yolo.add(tf.keras.layers.Conv2D(128, (3, 3), padding="same", kernel_regularizer=l2(5e-4), use_bias = False))
-yolo.add(tf.keras.layers.BatchNormalization()) #Batch normalization needs to be executed before lrelu apparently 
-yolo.add(LeakyReLU(alpha=0.1))
-yolo.add(tf.keras.layers.Conv2D(64, (3, 3), padding="same", kernel_regularizer=l2(5e-4), use_bias = False))
-yolo.add(tf.keras.layers.BatchNormalization()) #Batch normalization needs to be executed before lrelu apparently 
-yolo.add(LeakyReLU(alpha=0.1))
-yolo.add(tf.keras.layers.Conv2D(32, (3, 3), padding="same", kernel_regularizer=l2(5e-4), use_bias = False))
-yolo.add(tf.keras.layers.BatchNormalization()) #Batch normalization needs to be executed before lrelu apparently 
-yolo.add(LeakyReLU(alpha=0.1))
-yolo.add(tf.keras.layers.Conv2D(16, (3, 3), padding="same", kernel_regularizer=l2(5e-4), use_bias = False))
-yolo.add(tf.keras.layers.BatchNormalization()) #Batch normalization needs to be executed before lrelu apparently 
-yolo.add(LeakyReLU(alpha=0.1))
-yolo.add(tf.keras.layers.Conv2D(5, (3, 3), padding="same", activation = "sigmoid", kernel_regularizer=l2(5e-4), use_bias = False))
+yolo.add(Flatten())
+yolo.add(Dense(128))
+yolo.add(Dense(256))
+yolo.add(BatchNormalization())
+yolo.add(Dense(245, activation='sigmoid'))
 
+yolo.add(Reshape((7,7,5), input_shape=(245,)))
+ 
 yolo.summary()
